706f026116dfa21d3dbbac7f01c86a6a29edcb41
Eddy <eddy.castro@epn.edu.ec>
2026-01-16 17:38:50 -0500

    Cambios a Employee para compatibilidad

--- src/main/java/com/viraj/sample/entity/Employee.java
+++ src/main/java/com/viraj/sample/entity/Employee.java
@@ -1,11 +1,9 @@
 package com.viraj.sample.entity;
-
 import javax.persistence.*;
 
 @Entity
 @Table(name = "employee")
 public class Employee {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -16,40 +14,12 @@ public class Employee {
     @Column(name = "employee_dept")
     private String dept;
 
-    public Employee() {
-    }
+    public Employee() {}
 
-    public Employee(String name, String dept) {
-        this.name = name;
-        this.dept = dept;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDept() {
-        return dept;
-    }
-
-    public void setDept(String dept) {
-        this.dept = dept;
-    }
-
-    @Override
-    public String toString() {
-        return "Employee{id=" + id + ", name='" + name + "', dept='" + dept + "'}";
-    }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getDept() { return dept; }
+    public void setDept(String dept) { this.dept = dept; }
 }
